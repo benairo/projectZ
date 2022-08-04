f8546e2d56be98a1b173bcd7ee8acdc1982c0edf
benairo <ben.brighton.airo@gmail.com>
2022-08-04 17:23:58 +0100

    Add blood hit effect to test weapon

---
+++ Assets/Scripts/CrosshairTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairTarget : MonoBehaviour
+{
+    private Camera _mainCamera;
+    private Ray _ray;
+    private RaycastHit _hitInfo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _ray.origin = _mainCamera.transform.position;
+        _ray.direction = _mainCamera.transform.forward;
+        Physics.Raycast(_ray, out _hitInfo);
+        transform.position = _hitInfo.point;
+    }
+}
--- Assets/Scripts/RaycastWeapon.cs
+++ Assets/Scripts/RaycastWeapon.cs
@@ -7,13 +7,32 @@ public class RaycastWeapon : MonoBehaviour
     public bool isFiring = false;
 
     public ParticleSystem[] muzzleFlash;
-    
+
+    public ParticleSystem hitEffect;
+
+    public Transform raycastOrigin;
+
+    public Transform raycastDestination;
+
+    private Ray _ray;
+
+    private RaycastHit _hitInfo;
+
     public void StartFiring()
     {
         isFiring = true;
         foreach (var particle in muzzleFlash)
         {
             particle.Emit(1);
+        }
+
+        _ray.origin = raycastOrigin.position;
+        _ray.direction = raycastDestination.position - raycastOrigin.position;
+        if (Physics.Raycast(_ray, out _hitInfo))
+        {
+            hitEffect.transform.position = _hitInfo.point;
+            hitEffect.transform.forward = _hitInfo.normal;
+            hitEffect.Emit(1);
         }
     }
 
