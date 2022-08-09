f6a203ad23691eb229c8c1484799e2874afbb54b
benairo <ben.brighton.airo@gmail.com>
2022-08-09 16:58:14 +0100

    Added a health script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth;
+    [HideInInspector]
+    public float currentHealth;
+
+    private RagDoll _ragDoll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ragDoll = GetComponent<RagDoll>();
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0.0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        _ragDoll.ActivateRagDoll();
+    }
+}
