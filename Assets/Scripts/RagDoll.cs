2e4ca5c8cce1ae232c78b9bab5229b2c704e70bb
benairo <ben.brighton.airo@gmail.com>
2022-08-09 16:49:17 +0100

    Add ragdoll

---
+++ Assets/Scripts/RagDoll.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDoll : MonoBehaviour
+{
+    
+    private Rigidbody[] _rigidBodies;
+    private Animator _animator;
+    void Start()
+    {
+        _rigidBodies = GetComponentsInChildren<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        DeactivateRagDoll();
+    }
+
+    public void DeactivateRagDoll()
+    {
+        foreach (var rigidBody in _rigidBodies)
+        {
+            rigidBody.isKinematic = true;
+        }
+
+        _animator.enabled = true;
+    }
+
+    public void ActivateRagDoll()
+    {
+        foreach (var rigidBody in _rigidBodies)
+        {
+            rigidBody.isKinematic = false;
+        }
+
+        _animator.enabled = false;
+    }
+    
+}
