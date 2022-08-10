be691440438ad6e4887292840135d3bf834ad4d5
benairo <ben.brighton.airo@gmail.com>
2022-08-10 21:26:48 +0100

    Add a vignette effect when player takes damage

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerHealth : Health
 {
-    
+    private PlayerAiming _aiming;
+
+    private VolumeProfile _postProcessing;
     protected override void OnStart()
     {
-        
+        _aiming = GetComponent<PlayerAiming>();
+        _postProcessing = FindObjectOfType<Volume>().profile;
     }
 
     protected override void OnDamage(Vector3 direction)
     {
-        
+        Vignette vignette;
+        if (_postProcessing.TryGet(out vignette))
+        {
+            float percent = 1.0f - (currentHealth / maxHealth);
+            vignette.intensity.value = percent * 0.7f;
+        }
     }
 
     protected override void OnDeath(Vector3 direction)
     {
+        _aiming.enabled = false;
         Debug.Log("You have died!");
     }
 }
