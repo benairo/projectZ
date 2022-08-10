912b6df6ae60c6aed785475f3f9da785fcaf832a
benairo <ben.brighton.airo@gmail.com>
2022-08-10 18:37:02 +0100

    Small bug fixes + refactor health script to allow for player & zombie health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -8,13 +8,10 @@ public class Health : MonoBehaviour
     [HideInInspector]
     public float currentHealth;
     
-    private ZombieAgent _agent;
-
     private UIHealthBar _healthBar;
 // Start is called before the first frame update
     void Start()
-    {
-        _agent = GetComponent<ZombieAgent>();
+    { 
         currentHealth = maxHealth;
         _healthBar = GetComponentInChildren<UIHealthBar>();
 
@@ -24,12 +21,15 @@ public class Health : MonoBehaviour
             HitBox hitBox = rigidBody.gameObject.AddComponent<HitBox>();
             hitBox.health = this;
         }
+        
+        OnStart();
     }
 
     public void TakeDamage(float amount, Vector3 direction)
     {
         currentHealth -= amount;
         _healthBar.SetHealthBarPercentage(currentHealth / maxHealth);
+        OnDamage(direction);
         if (currentHealth <= 0.0f)
         {
             Die(direction);
@@ -38,8 +38,21 @@ public class Health : MonoBehaviour
 
     private void Die(Vector3 direction)
     {
-        ZombieDeathState deathState = _agent.stateMachine.GetState(ZombieStateID.Death) as ZombieDeathState;
-        deathState.direction = direction;
-        _agent.stateMachine.ChangeState(ZombieStateID.Death);
+        OnDeath(direction);
+    }
+
+    protected virtual void OnStart()
+    {
+        
+    }
+
+    protected virtual void OnDamage(Vector3 direction)
+    {
+        
+    }
+
+    protected virtual void OnDeath(Vector3 direction)
+    {
+        
     }
 }
---
+++ Assets/Scripts/ZombieHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieHealth : Health
+{
+    private ZombieAgent _agent;
+    protected override void OnStart()
+    {
+        _agent = GetComponent<ZombieAgent>();
+    }
+
+    protected override void OnDamage(Vector3 direction)
+    {
+        
+    }
+
+    protected override void OnDeath(Vector3 direction)
+    {
+        ZombieDeathState deathState = _agent.stateMachine.GetState(ZombieStateID.Death) as ZombieDeathState;
+        deathState.direction = direction;
+        _agent.stateMachine.ChangeState(ZombieStateID.Death);
+    }
+}
