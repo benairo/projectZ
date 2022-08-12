83cb9b23ee897ddf07a41d26c940364dbd7d03ab
benairo <ben.brighton.airo@gmail.com>
2022-08-12 16:39:52 +0100

    Fix a bug with playermovement updating at the same time as animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,11 +19,16 @@ public class PlayerMovement : MonoBehaviour
 
     private bool _isGamePad;
 
+    private Vector3 _rootMotion;
+
+    private CharacterController _charController;
+
     private void Awake()
     {
         _playerControls = new PlayerControls();
         _playerInput = GetComponent<PlayerInput>();
         _animator = GetComponent<Animator>();
+        _charController = GetComponent<CharacterController>();
     }
 
     private void OnEnable()
@@ -43,10 +48,17 @@ public class PlayerMovement : MonoBehaviour
         _animator.SetFloat("InputX", _input.x);
         _animator.SetFloat("InputY", _input.y);
 
+        _charController.Move(_rootMotion);
+        _rootMotion = Vector3.zero;
+
     }
-    
     public void OnDeviceChange(PlayerInput pi)
     {
         _isGamePad = pi.currentControlScheme.Equals("Gamepad") ? true : false;
+    }
+
+    private void OnAnimatorMove()
+    {
+        _rootMotion += _animator.deltaPosition;
     }
 }
