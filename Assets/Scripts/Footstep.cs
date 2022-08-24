82f2201ef572ab744dd1b00391451278cc27ff02
benairo <ben.brighton.airo@gmail.com>
2022-08-24 15:43:10 +0100

    Add back player footsteps using animation events

---
+++ Assets/Scripts/Footstep.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footstep : MonoBehaviour
+{
+    public AudioSource source;
+
+    public AudioClip footstep;
+
+    void Step()
+    {
+        source.clip = footstep;
+        source.Play();
+    }
+}
